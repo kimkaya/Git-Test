9b9e6fadd061e595e25b2ee289631271d20f3e50
(nagunho) <(sanikawakayari@naver.com)>
2022-11-03 13:44:39 +0900

    boardservice logic

--- src/main/java/miniproject/board/repository/JpaBoardRepository.java
+++ src/main/java/miniproject/board/repository/JpaBoardRepository.java
@@ -1,8 +1,13 @@
 package miniproject.board.repository;
 
+import miniproject.board.domain.Boards;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface JpaBoardRepository extends JpaRepository {
+public interface JpaBoardRepository extends JpaRepository<Boards , Integer> {
+
+
 }
--- src/main/java/miniproject/board/service/BoardService.java
+++ src/main/java/miniproject/board/service/BoardService.java
@@ -1,8 +1,39 @@
 package miniproject.board.service;
 
+import miniproject.board.domain.Boards;
+import miniproject.board.repository.JpaBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoardService {
-    private String board;
+    @Autowired
+    private final JpaBoardRepository repository;
+
+    public BoardService(JpaBoardRepository repository) {
+        this.repository = repository;
+    }
+
+    public Integer BoardSave(Boards boards){
+        Boards save = repository.save(boards);
+        return save.getId();
+    }
+
+    public List<Boards> boardsList(){
+        List<Boards> boards = repository.findAll();
+        return boards;
+    }
+
+    public  void boardDelete(Integer id){
+        repository.deleteById(id);
+    }
+
+    public Boards boards(Integer id){
+        Optional<Boards> boardsList = repository.findById(id);
+        return boardsList.get();
+    }
+
 }
