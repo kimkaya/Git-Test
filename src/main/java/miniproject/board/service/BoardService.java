4eb408057940d07afab2b4aca6b5fbc6e505ee10
(nagunho) <(sanikawakayari@naver.com)>
2022-11-04 12:21:51 +0900

    boardlist page 정리 위해 Pageable 추가 Controller 연결 준비

--- src/main/java/miniproject/board/service/BoardService.java
+++ src/main/java/miniproject/board/service/BoardService.java
@@ -3,6 +3,8 @@ package miniproject.board.service;
 import miniproject.board.domain.Boards;
 import miniproject.board.repository.JpaBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,8 +23,8 @@ public class BoardService {
        repository.save(boards);
     }
 
-    public List<Boards> boardsList(){
-        List<Boards> boards = repository.findAll();
+    public Page<Boards> boardsList(Pageable pageable){
+        Page<Boards> boards = repository.findAll(pageable);
         return boards;
     }
 
